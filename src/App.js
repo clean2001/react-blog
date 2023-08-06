14b8f5ff28df4cc7e9b468d88ecccb6ea9faa6fd
kimsejeong <clean01@hanyang.ac.kr>
2023-08-07 03:52:25 +0900

    UDT: Header 기본틀

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.tsx';
-
+import styles from "./components/app.module.css";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <div className="header">
+        <span className={styles.title}>Clean Page</span>
+        <span className={styles.headerBtn}>Main</span>
+        <span className={styles.headerBtn}>Info</span>
+        <span className={styles.headerBtn}>Memo</span>
+        <span className={styles.headerBtn}>Gallery</span>
+
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, useRef, useState} from "react";
+import React, { ReactNode, useRef, useState } from "react";
 import styles from "./header.module.css";
 
 export default function Header() {
