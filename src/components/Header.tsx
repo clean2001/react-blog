5f5e7980b8fc8950f93706d1a39a4969c647ecbf
kimsejeong <clean01@hanyang.ac.kr>
2023-08-07 00:13:25 +0900

    CRET: react app

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React, {ReactNode, useRef, useState} from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+    return (
+        <div id="header">
+            <span className={styles.title}>Blog</span>
+        </div>
+    );
+}
